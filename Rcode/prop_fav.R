d64860634f91e5a4c44f9dd6892f921c57379c8b
Sebastian Sauer <ssauer@posteo.de>
2017-07-06 16:40:16 +0200

    prop_fav function

---
+++ Rcode/prop_fav.R
@@ -0,0 +1,80 @@
+
+
+prop_fav2 <- function(df, value, g1, g2){
+
+  if (!(require(tidyverse))) stop("package tidyverse is needed but not loaded.")
+
+  filter_clause1 <- quo(!!group == g1)
+  filter_clause2 <- quo(!!group == g2)
+
+  value <- enquo(value)
+
+  df %>%
+    filter(!!filter_clause1) %>%
+    select(!!value) %>%
+    pull -> values_g1
+
+  values_g1 <- na.omit(values_g1)
+
+  df %>%
+    filter(!!filter_clause2) %>%
+    select(!!value) %>%
+    pull -> values_g2
+
+  values_g2 <- na.omit(values_g2)
+
+  comp_grid <- expand.grid(values_g1, values_g2)
+
+  fav_vec <- comp_grid[["Var1"]] > comp_grid[["Var2"]]
+
+  fav_sum <- sum(fav_vec)
+
+  fav_prop <- fav_sum / nrow(comp_grid)
+
+  return(fav_prop)
+
+}
+
+
+simpleFunction <- function(dataset, col_name, value){
+  col_name <- enquo(col_name)
+  dataset %>%
+    filter((!!col_name) == value) %>%
+    summarise(mean_cyl = mean(cyl)) -> dataset
+  return(dataset)
+}
+simpleFunction(mtcars, am, 1)
+
+
+
+
+prop_fav <- function(df, value, group, g1, g2){
+  value <- enquo(value)
+  group <- enquo(group)
+
+
+  df %>%
+   filter((!!group) == g1) %>%
+   select(!!value) %>%
+   pull -> values_g1
+
+  df %>%
+    filter((!!group) == g2) %>%
+    select(!!value) %>%
+    pull -> values_g2
+
+  values_g1 <- na.omit(values_g1)
+
+  comp_grid <- expand.grid(values_g1, values_g2)
+
+  fav_vec <- comp_grid[["Var1"]] > comp_grid[["Var2"]]
+
+  fav_sum <- sum(fav_vec)
+
+  fav_prop <- fav_sum / nrow(comp_grid)
+
+  return(fav_prop)
+
+}
+
+prop_fav(extra, extra_mean, sex, "Frau", "Mann")
